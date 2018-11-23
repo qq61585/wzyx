9939db08e4e065a07b2858fe51882fc7350a6be9
fengjl <615853674@qq.com>
2018-11-24 00:02:28 +0800

    增加了管理员维护个人信息和用户信息的接口

---
+++ src/main/java/com/wzyx/util/FTPUtil.java
@@ -0,0 +1,23 @@
+package com.wzyx.util;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * FTP工具类，用于向FTP服务器上传文件，主要是图片
+ */
+public class FTPUtil {
+
+    private static Logger logger = LoggerFactory.getLogger(FTPUtil.class);
+
+    private static String ftpIP = PropertiesUtil.getProperty("ftp.server.ip");
+    private static String ftpUser = PropertiesUtil.getProperty("ftp.user");
+    private static String ftpPassword = PropertiesUtil.getProperty("ftp.password");
+
+
+
+
+    private
+
+}
