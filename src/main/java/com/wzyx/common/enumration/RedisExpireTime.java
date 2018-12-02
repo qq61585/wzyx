d8ad4d5340b0790fb688742f630a55dcd06d53d4
fengjl <615853674@qq.com>
2018-12-02 15:27:12 +0800

    更新移动端用户失效时间

--- src/main/java/com/wzyx/common/enumration/RedisExpireTime.java
+++ src/main/java/com/wzyx/common/enumration/RedisExpireTime.java
@@ -5,7 +5,7 @@ package com.wzyx.common.enumration;
  */
 public enum RedisExpireTime {
 
-    USER_EXPIRE_TIME(60 * 30, "用户登录失效时间，30分钟"),
+    USER_EXPIRE_TIME(60 * 60 * 24 * 7, "移动端的用户登录失效时间，7天"),
     VERIFICATION_CODE_EXPIRE_TIME(60 * 10, "验证码失效时间，10分钟");
 
     private int time;
